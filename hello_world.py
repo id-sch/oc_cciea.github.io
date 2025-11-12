799f927217812e8caced1e0106f7faf9ef2416e6
Isaac Schroeder <isaac.schroeder@noaa.gov>
2025-11-12 12:05:58 -0800

    Changed hello_world to write an org file.

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,7 @@
 # hello_world.py
 print("Hello, World!")
+
+file = open("hello_world.org", "w")
+file.write("* Hello World\n+this is my first file created using Python!")
+file.close()
+# print("File created successfully!")
