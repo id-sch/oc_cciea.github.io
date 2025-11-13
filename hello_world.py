0512f85cb74a6c08eee7e709dbdf3830cdde1f1c
Isaac Schroeder <isaac.schroeder@noaa.gov>
2025-11-13 10:26:08 -0800

    Changed the org file contents.

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,6 @@
 print("Hello, World!")
 
 file = open("hello_world.org", "w")
-file.write("* Hello World\n+this is my first file created using Python!\n+you need to set read/write permissions in Actions/General on Github")
+file.write("* Hello World\n**this is my first file created using Python!\n**you need to set read/write permissions in Actions/General on Github")
 file.close()
 print("File created successfully!")
