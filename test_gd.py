8cf091e32016046917c15500010fcbc615208d8a
Isaac Schroeder <isaac.schroeder@noaa.gov>
2025-11-28 09:10:54 -0800

    Testing auth google.

---
+++ test_gd.py
@@ -0,0 +1,13 @@
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+drive = GoogleDrive(gauth)
+
+
+
+file1 = drive.CreateFile({'title': 'Hello1.txt'})
+file1.SetContentString('Hello1')
+file1.Upload() # Files.insert()
