185fcc378dbe7d6d9c0b85cac4f7aa1e9d9a6e49
Isaac Schroeder <isaac.schroeder@noaa.gov>
2025-12-04 09:45:06 -0800

    New test for gd upload.

---
+++ test_gd_upload.py
@@ -0,0 +1,15 @@
+import os
+
+
+# --create output directory
+dir_out = 'dir_test_gd'
+
+# --check if directory exist, if it doesn't then create
+try:
+    os.makedirs(dir_out)
+except OSError:
+    if not os.path.isdir(dir_out):
+        raise
+
+with open('{}/readme.txt'.format(dir_out), 'w') as f:
+    f.write('Create a new text file!')
