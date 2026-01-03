ca203cdcd55668ca7005d72c6499dfecf895628b
Isaac Schroeder <isaac.schroeder@noaa.gov>
2026-01-02 20:29:09 -0800

    Changed name of the seasonal file to create CSV files.

--- code_gha/OceanAcidification/create_oc_arag_nh_S_std.py
+++ code_gha/OceanAcidification/create_oc_arag_nh_S_std.py
@@ -1,6 +1,11 @@
 import xarray as xr
+import numpy as np
 import pandas as pd
+# import os
+import re
 from fun_pd_df2csvR_time import fun_pd_df2csvR_time
+
+
 
 
 # ----------------------------------------------------------------------
@@ -47,11 +52,6 @@ yr_csv_bgn = 1998
 # --END: Change These
 # ----------------------------------------------------------------------
 
-df_list = []
-ts_lbl = []
-depth_list = []
-lon_list = []
-lat_list = []
 for i in range(0, num_sttn):
     # --create input filename
     fn_in = dir_in + sttn_wnt[i] + fn_excel_in + file_type_in
@@ -68,10 +68,6 @@ for i in range(0, num_sttn):
     lat = dall.attrs[att_wnt[2]]
     lon = dall.attrs[att_wnt[3]]
 
-    depth_list.append(depth)
-    lon_list.append(lon)
-    lat_list.append(lat)
-
     # --station label
     line_sttn_lbl = station
 
@@ -81,30 +77,47 @@ for i in range(0, num_sttn):
     # --variable as a pd df
     dfi = dall[var_wnt].to_dataframe()
 
-    # --resample by taking 'ME' means, i.e. monthly means
-    dfMmn = dfi.resample('ME').mean()
-    dfMsd = dfi.resample('ME').std()
+    # --resample by taking seasonal means
+    dfQmn = dfi.resample('QE-MAR').mean()
+    dfQsd = dfi.resample('QE-MAR').std()
 
     # --combine mean and sd into one pd.df
-    dfM = dfMmn.rename(index=str, columns={var_wnt: "data"})
-    dfM['Datetime'] = pd.to_datetime(dfM.index)
-    dfM = dfM.set_index('Datetime')
+    dfQ = dfQmn.rename(index=str, columns={var_wnt: "data"})
+    dfQ['Datetime'] = pd.to_datetime(dfQ.index)
+    dfQ = dfQ.set_index('Datetime')
 
-    dfM['sd'] = dfMsd.values
-    ts_lbl1 = 'Monthly ' + ttl_lbl + \
+    dfQ['sd'] = dfQsd.values
+
+    in1 = dfQ.index.month == 3
+    in2 = dfQ.index.month == 6
+    in3 = dfQ.index.month == 9
+    in4 = dfQ.index.month == 12
+    dfQ1 = dfQ[in1]
+    dfQ2 = dfQ[in2]
+    dfQ3 = dfQ[in3]
+    dfQ4 = dfQ[in4]
+    ts_lbl1 = 'Winter ' + ttl_lbl + \
+        ' at {}'.format(depth.astype(int)) + ' m: ' + line_sttn_lbl + loc_lbl
+    ts_lbl2 = 'Spring ' + ttl_lbl + \
+        ' at {}'.format(depth.astype(int)) + ' m: ' + line_sttn_lbl + loc_lbl
+    ts_lbl3 = 'Summer ' + ttl_lbl + \
+        ' at {}'.format(depth.astype(int)) + ' m: ' + line_sttn_lbl + loc_lbl
+    ts_lbl4 = 'Fall ' + ttl_lbl + \
         ' at {}'.format(depth.astype(int)) + ' m: ' + line_sttn_lbl + loc_lbl
 
-    df_list.append(dfM)
-    ts_lbl.append(ts_lbl1)
+    # --Create monthly CSV, 'fun_pd_df2csvR' expects a list of pd.df. For
+    # --the case of monthly means this list only consists of dfQ, but a
+    # --CSV for seasonal means will need a list of 4 pd.df for each season.
+    df_list = [dfQ1, dfQ2, dfQ3, dfQ4]
+    num_order = len(df_list)
 
-# file name and save the CSV
-fn_out = '{}_M.csv'.format(file_pre)
+    fn_out = file_pre + '_' + sttn_wnt[i] + '_S.csv'
 
-metric_lbl = y_lbl
+    metric_lbl = y_lbl
+    ts_lbl = [ts_lbl1, ts_lbl2, ts_lbl3, ts_lbl4]
+    clmns_iea = ['year', 'time', 'index', 'error', 'SElo', 'SEup',
+                 'metric', 'timeseries', 'lat', 'lon', 'depth', 'order']
 
-clmns_iea = ['year', 'month', 'time', 'data', 'error', 'SElo', 'SEup',
-             'metric', 'timeseries', 'lat', 'lon', 'depth', 'order']
-
-fn_out_csv = fun_pd_df2csvR_time(
-    clmns_iea, df_list, lat_list, lon_list, depth_list, metric_lbl, ts_lbl,
-    dir_out, fn_out, yr_csv_bgn, yr_csv_end)
+    fn_out_csv = fun_pd_df2csvR_time(
+        clmns_iea, df_list, lat, lon, depth, metric_lbl, ts_lbl, dir_out,
+        fn_out, yr_csv_bgn, yr_csv_end)
