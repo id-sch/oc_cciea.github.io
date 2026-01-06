700634a0c2be359980373ffd0ae90c3d7971f6f9
Isaac Schroeder <isaac.schroeder@noaa.gov>
2026-01-06 15:49:47 -0800

    TIme x depth arag sat firgures for 2024.

--- code_gha/OceanAcidification/arag_time_depth_nh.py
+++ code_gha/OceanAcidification/arag_time_depth_nh.py
@@ -18,7 +18,7 @@ mpl.rcParams.update({'font.size': 12})
 # BEGIN: Change These
 # ----------------------------------------------------------------------
 # iea year
-iea_yr = 2025
+iea_yr = 2024
 
 file_pre = 'oc_arg_Newport'
 
