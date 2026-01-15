5d599b69e2cdfd799855a2d5c9885a5716109e51
Isaac Schroeder <isaac.schroeder@noaa.gov>
2026-01-14 18:16:25 -0800

    Plot 2025 phenology time series.

--- code_gha/phenologyUI/plot_cui_for_esr.py
+++ code_gha/phenologyUI/plot_cui_for_esr.py
@@ -18,7 +18,7 @@ mpl.rcParams.update({'font.size': 12})
 # --BEGIN: Change These
 # ----------------------------------------------------------------------
 # iea year
-iea_yr = 2024
+iea_yr = 2025
 
 # --basin index wanted
 # set lat range
